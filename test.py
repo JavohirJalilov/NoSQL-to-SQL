de101c731bf6fb26a48237c2cb2ea60044723d89
JavohirJalilov <javohirjaliloff@gmail.com>
2023-05-20 19:16:56 +0500

    post

--- test.py
+++ test.py
@@ -3,17 +3,19 @@ from tinydb import TinyDB
 db = TinyDB('data.json')
 
 tables = list(db.tables())
-table = db.table(tables[0])
+table = db.table(tables[1])
 
 data = table.all()
 
 url = 'http://127.0.0.1:8000/api/add/'
 print(data[0])
 for i in data:
+    idx1 = i['RAM'].find('G')
+    idx2 = i['memory'].find('G')
     data = {
         "price": i['price'],
-        "memory": i['memory'],
-        "ram": i['RAM'],
+        "memory": int(i['memory'][:idx2]),
+        "ram": int(i['RAM'][:idx1]),
         "color": i['color'],
         "img_url": i['img_url'],
         "name": i['name'],
